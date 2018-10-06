eead707a5b773eb90bc8888970f801606d217e0c
Perry Tran <ptran209@gmail.com>
2018-10-06 13:35:08 -0700

    init commit

--- src/components/app.js
+++ src/components/app.js
@@ -1,13 +1,10 @@
 import React from 'react';
-import '../assets/css/app.css';
-import logo from '../assets/images/logo.svg';
 
 const App = () => (
-    <div>
-        <div className="app">
-            <img src={logo} className="logo rotate"/>
-            <h1>Welcome to React</h1>
-        </div>
+    <div className="container">
+        <div className="row">
+            <h1>Hello World</h1>
+        </div>     
     </div>
 );
 
